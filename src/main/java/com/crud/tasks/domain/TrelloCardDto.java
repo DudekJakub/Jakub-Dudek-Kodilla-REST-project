312c08d618377d28e20bc12ac7f91180d9a32995
jakub <jakub.dudek94@gmail.com>
2022-04-23 20:48:01 +0200

    Json properties FIX vol.7

--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -14,9 +14,18 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloCardDto {
 
+    @JsonProperty("id")
     private String id;
+
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("desc")
     private String desc;
+
+    @JsonProperty("pos")
     private String pos;
+
+    @JsonProperty("idList")
     private String idList;
 }
